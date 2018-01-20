c46becd02460127cd99ff316f38ed68aea510bce
Ugo Arzur <ugo.arzur@gmail.com>
2018-01-20 18:44:38 +0100

    edit script build wp

--- scripts/3-build_wp.sh
+++ scripts/3-build_wp.sh
@@ -3,19 +3,17 @@ cd wordpress/
 echo "--- Creating an image for wordpress"
 
 echo "--- Generate Dockerfile "
+echo $(pwd)
 cat << EDL >> Dockerfile
 FROM nginx:latest
-# COPY ./scripts/wp-install.sh /var/html/www/wp-install.sh
+COPY ../scripts/2-wp-install.sh /var/html/www/wp-install.sh
 WORKDIR /var/html/www/
 #RUN sh wp-install.sh
 EXPOSE 80 443
 EDL
 
-echo "--- Move Dockerfile to wordpress "
-mv Dockerfile wordpress/
-
 echo "--- Building image "
-cd wordpress && docker build -t shrimp .
-
+docker build -t shrimp .
+echo "--- Image built "
 docker images | grep shrimp
 # docker push shrimp
